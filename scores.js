002feeb01abade6ee721082cc7efc556cbc8407f
EthanMCox <ethancox@byu.edu>
2024-02-20 22:37:51 -0700

    Finish implementing loadScores Logic

--- scores.js
+++ scores.js
@@ -18,7 +18,30 @@ function loadScores() {
 
   // Check if there are scores to display
   if(scores.length) {
-    return;
+    // If there are scores, loop through each score and create a table row for each
+    for (const [i, score] of scores.entries()) {
+      // Create table cells for ran, name, score, and date
+      const rankTdEl = document.createElement('td');
+      const nameTdEl = document.createElement('td');
+      const scoreTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
+
+      // Set the content of each table ceel with the corresponding score data
+      rankTdEl.textContent = i + 1; // Rank is index + 1
+      nameTdEl.textContent = score.name;
+      scoreTdEl.textContent = score.score;
+      dateTdEl.textContent = score.date;
+
+      // Create a table row and append the table cells to it
+      const rowEl = document.createElement('tr');
+      rowEl.appendChild(rankTdEl);
+      rowEl.appendChild(nameTdEl);
+      rowEl.appendChild(scoreTdEl);
+      rowEl.appendChild(dateTdEl);
+
+      // Append the row to the table body
+      tableBodyEl.appendChild(rowEl);
+    }
   } else { // If there are no scores, dispaly a message spanning four columns
     tableBodyEl.innerHTML = "<tr><td colSpan=4>Be the first to score</td></tr>";
   }
