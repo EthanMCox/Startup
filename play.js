9ac9961c30ed08faf54559309f435ea4ca5da460
EthanMCox <ethancox@byu.edu>
2024-02-22 20:54:00 -0700

    Added base Game class and username retrieval from local storage

--- play.js
+++ play.js
@@ -1,4 +1,35 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Select all elements with the class .card
+//   var cards = document.querySelectorAll(".card");
+
+//   // Add a click event lister to each card
+//   cards.forEach((card) => {
+//     card.addEventListener("click", () => {
+//       // Add the class .flipped to the card
+//       this.querySelector('.card-inner').classList.toggle("flipped");
+//     });
+
+// });
+
+
 
 class Game {
-  
+  allowPlayer;
+
+  constructor() {
+    this.allowPlayer = false;
+
+    const playerNameEl = document.querySelector('.player-name');
+    playerNameEl.textContent = this.getPlayerName();
+  }
+
+  getPlayerName() {
+    return localStorage.getItem('userName') ?? 'Mystery Player';
+  }
+
+
 }
+
+const game = new Game();
+
+
