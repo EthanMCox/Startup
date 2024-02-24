435f0c7a6ea52953e55b137818ec1a95ac76ea8e
EthanMCox <ethancox@byu.edu>
2024-02-23 20:28:04 -0700

    Added interval for score display as websocket placeholder

--- play.js
+++ play.js
@@ -89,5 +89,10 @@ class Game {
 
 const game = new Game();
 
-
+setInterval(() => { // WebSocket placeholder
+const score = Math.floor(Math.random() * 3000);
+const chatText = document.querySelector('#notification');
+chatText.innerHTML = 
+`<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` + chatText.innerHTML;
+}, 20000);
 
