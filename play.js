3e02def69c70a9271cbc32c4d7e5240369882521
EthanMCox <ethancox@byu.edu>
2024-02-27 19:44:53 -0700

    Added conditions for matching all cards and running out of lives

--- play.js
+++ play.js
@@ -45,23 +45,24 @@ class Game {
   cards;
   allowPlayer;
   cardsflipped;
+  cardsmatched;
   score;
   lives;
+  round;
 
   constructor() {
     this.allowPlayer = true;
     this.cards = new Map();
     this.cardsflipped = []
+    this.cardsmatched = 0;
     this.score = 0
     this.lives = 10;
+    this.round = 1;
 
     this.createCards();
 
     const playerNameEl = document.querySelector('.player-name');
     playerNameEl.textContent = this.getPlayerName();
-    // console.log(this.cards);
-    // this.saveScore(6); // For testing save score functionality
-    // this.updatelives(3);
   }
 
   updatelives() {
@@ -69,9 +70,14 @@ class Game {
     livesEl.value = this.lives;
   }
 
-  updateScore(score) {
+  updateround() {
+    const roundEl = document.querySelector('#round');
+    roundEl.value = this.round;
+  }
+
+  updateScore() {
     const scoreEl = document.querySelector('#score');
-    scoreEl.value = score;
+    scoreEl.value = this.score;
   }
 
   getPlayerName() {
@@ -96,34 +102,50 @@ class Game {
   async clickcard(card) {
     // Only execute if the player is allowed to flip the card
     if (this.allowPlayer === false || card.allowFlip === false) return;
+    // ensure players can't flip another card while this card flip executes
     this.allowPlayer = false;
     card.allowFlip = false;
 
     // Flip the card and add to cardsflipped array
     this.cardsflipped.push(card);
     this.flipcard(card);
-    await delay(1000);
+    await delay(800);
 
     // If two cards are flipped, check if they match. Flip them back and set lives to 1 less if they don't match. Otherwise, update the score and set cards to display none
     if (this.cardsflipped.length === 2) { 
+      cardsmatched = cardsmatched + 2;
       if (this.cardsflipped[0].letter === this.cardsflipped[1].letter) { // Found a match
-        this.updateScore(this.score + 1);
-        await delay(200);
+        this.score = this.score + 1;
+        this.updateScore();
+        await delay(400);
         this.cardsflipped[0].el.style.display = "none"; // Change to something more elegant later if time
         this.cardsflipped[1].el.style.display = "none";
       }
       else { // No match
         this.lives = this.lives - 1;
         this.updatelives();
-        await delay(300);
         this.flipcard(this.cardsflipped[0]);
         this.flipcard(this.cardsflipped[1]);
-        await delay(1200);
+        await delay(1000);
         this.cardsflipped[0].allowFlip = true;
         this.cardsflipped[1].allowFlip = true;
       }
       this.cardsflipped = [];
     }
+    
+    // If all cards are matched, generate more cards and increment round
+    if (cardsmatched === 12) {
+      this.round = this.round + 1;
+      this.updateround();
+      this.resetcards();
+    }
+    // If lives are 0, save the score and reset the game
+    if (this.lives === 0) {
+      this.saveScore(this.score);
+      this.restart();
+    }
+
+
     this.allowPlayer = true;
   }
 
@@ -185,7 +207,10 @@ class Game {
 
   async restart() {
     this.allowPlayer = false;
-    this.updateScore("--");
+    // Updates score to be "--"
+    const scoreEl = document.querySelector('#score');
+    scoreEl.value = "--";
+
     this.resetcards();
     this.allowPlayer = true;
   }
