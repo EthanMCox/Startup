d765c0f4501d483f82a9ea118d9d4a79f47a2f36
EthanMCox <ethancox@byu.edu>
2024-03-07 16:18:59 -0700

    Created API router for endpoints

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 express = require('express');
-// const express = require('express');
+
 const app = express();
 
+// Use the port provided as the first command-line argument, or 3000 if not provided
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
@@ -14,6 +15,12 @@ app.use(express.static('public'));
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
+
+// Create a router for the API Service Endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
 
 // Start the server, log where the server is running
 app.listen(port, () => {
