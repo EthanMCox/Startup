6b66d6e1bfcad5fe62c3db87c2ded1ca0ff427df
EthanMCox <ethancox@byu.edu>
2024-03-07 16:23:08 -0700

    Added updateScores function to index.js

--- index.js
+++ index.js
@@ -26,3 +26,24 @@ app.use(`/api`, apiRouter);
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
+
+let scores = [];
+function updateScores(newScore, scores) {
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+    }
+
+    // Insert score at the bottom of the list if not already found
+    if (!found) {
+        scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+        scores.length = 10;
+    }
+
+    return scores;
+}
