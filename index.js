53abcb048b3f42ec935f0059db62613aea33771a
EthanMCox <ethancox@byu.edu>
2024-03-07 16:11:04 -0700

    Corrected license on package.json

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import * as express from 'express';
 // const express = require('express');
 const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
