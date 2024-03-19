624d02541ecc8923a413237133d3a2c8fe8238b6
EthanMCox <ethancox@byu.edu>
2024-03-18 21:46:00 -0600

    Added require statements for database.js

--- database.js
+++ database.js
@@ -1,8 +1,17 @@
 const config = require('./dbConfig.json');
 const { MongoClient } = require('mongodb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
+
+
+
+
 
 (async function testConnection() {
   await client.connect();
