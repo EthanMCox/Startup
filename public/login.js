1da6c5402e815d597792dae218d82a6dd74568f7
EthanMCox <ethancox@byu.edu>
2024-03-22 11:15:22 -0600

    Added login, logout, createuser functionality in login.js

--- public/login.js
+++ public/login.js
@@ -1,7 +1,67 @@
-function login() {
-  const nameEl = document.querySelector("#name");
-  const passwordEl = document.querySelector("#password");
-  localStorage.setItem("userName", nameEl.value);
-  localStorage.setItem("password", passwordEl.value);
-  window.location.href = "play.html";
+// At runtime, check if userName is saved in localStorage and display the appropriate controls
+(async () => {
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('authenticationControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('authenticationControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
 }
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password}),
+    headers: {
+      'Content-Type': 'application/json, charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'play.html';
+  }
+}
+
+function play() {
+  window.location.href = 'play.html';
+}
+
+function logout() {
+  localStorage.removeItem('userName');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
+
