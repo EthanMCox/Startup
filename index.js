99171c4b5170fb8fa6f667debac541c582dfceb4
EthanMCox <ethancox@byu.edu>
2024-03-21 18:41:30 -0600

    Added index.js endpoint for logging in

--- index.js
+++ index.js
@@ -42,6 +42,19 @@ apiRouter.post('/auth/create', async (req, res) => {
     }
 });
 
+// Get Auth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ id: user._id});
+            return;
+        }
+    }
+    res.status(401).send({ msg: "Invalid email or password" });
+});
+
 // GetScores endpoint
 apiRouter.get('/scores', (_req, res) => {
     res.send(scores);
