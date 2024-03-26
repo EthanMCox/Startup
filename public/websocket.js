498870b0588994dbf6cf84dd872cdbca35c7edc2
EthanMCox <ethancox@byu.edu>
2024-03-25 22:45:18 -0600

    handle HTTP to WebSocket protocol upgrade

--- public/websocket.js
+++ public/websocket.js
@@ -5,5 +5,11 @@ function webSocketServer(httpServer) {
     // Create a websocket object
     const wss = new webSocketServer({ noServer: true});
 
-    // 
+    // Handle the protocol upgrade from HTTP to WebSocket
+    httpServer.on('upgrade', (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
+    });
 }
+
