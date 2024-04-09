b95a10447fd1eec6c2cb521d6c5b1411f8604153
EthanMCox <ethancox@byu.edu>
2024-04-08 22:03:08 -0600

    Added stub about, login, play, and scores components so the page can render

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { Login } from "./login/login";
-import { Play } from "./login/login";
+import { Play } from "./play/play";
 import { Scores } from "./scores/scores";
 import { About } from "./about/about";
 import { AuthState } from "./login/authState";
---
+++ src/play/play.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Play() {
+    return (
+        <div>hello</div>
+    );
+}
