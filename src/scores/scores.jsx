021841290f209c35e43e81f048a2f2a9e88560a0
EthanMCox <ethancox@byu.edu>
2024-04-11 15:39:36 -0600

    Finished transitioning scores page to react

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -5,6 +5,35 @@ import "./scores.css";
 export function Scores() {
   const [scores, setScores] = React.useState([]);
 
+  React.useEffect(() => { // On first loading of component, get the scores
+    fetch('api/scores')
+        .then((response) => response.json())
+        .then((scores) => {
+            setScores(scores);
+            localStorage.setItem('scores', JSON.stringify(scores));
+        });
+  }, []);
+
+  const scoreRows = [];
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      scoreRows.push(
+        <tr key={i}>
+          <td>{i+1}</td>
+          <td>{score.name.split('@')[0]}</td>
+          <td>{score.score}</td>
+          <td>{score.date}</td>
+        </tr>
+      );
+    }
+  } else {
+        scoreRows.push(
+        <tr>
+            <td colSpan="4">Be the first to score</td>
+        </tr>
+        );
+    }
+
   return (
     <main class="container-fluid bg-secondary text-center">
       <table class="table table-striped">
@@ -16,7 +45,7 @@ export function Scores() {
             <th>Date</th>
           </tr>
         </thead>
-        <tbody id="scores">Hello</tbody>
+        <tbody id="scores">{scoreRows}</tbody>
       </table>
     </main>
   );
