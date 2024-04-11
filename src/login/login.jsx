ade6cb1ba486f8c1ad53fc6872cbba31575f07f8
EthanMCox <ethancox@byu.edu>
2024-04-10 22:38:08 -0600

    Added authenticated.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Unauthenticated } from "./unauthenticated";
-// import { Authenticated } from "./authenticated";
+import { Authenticated } from "./authenticated";
 
 import { AuthState } from "./authState";
 
-
-export function Login({userName, authState, onAuthChange}) {
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container-fluid bg-secondary text-center">
       <div>
         <h2>Welcome to Make a Match</h2>
+        {authState === AuthState.Authenticated && (
+          <Authenticated
+            userName={userName}
+            onLogout={() => {
+              onAuthChange(userName, AuthState.Unauthenticated);
+            }}
+          />
+        )}
         {authState === AuthState.Unauthenticated && (
           <Unauthenticated
             userName={userName}
