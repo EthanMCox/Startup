fc3fc4cd6678a513d53afb4d2897626c5ad1a3a0
EthanMCox <ethancox@byu.edu>
2024-04-12 11:39:23 -0600

    Added html structure to card.jsx

--- src/play/card.jsx
+++ src/play/card.jsx
@@ -1,3 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export function Card(props) {
+  return (
+    <div class="card-center">
+      <div class="card" id="card1">
+        <div class="card-inner">
+          <div class="transparent">
+            <img class="card-img-top" src="card.jpg" alt="playing_card" />
+          </div>
+          <div class="card-front">
+            <img class="card-img-top" src="card.jpg" alt="playing_card" />
+          </div>
+          <div class="card-back">
+            <div>
+              <p class="card-back-text">A</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
