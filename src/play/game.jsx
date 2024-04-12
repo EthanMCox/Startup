3811c76c8dd3d892529eb89f14a46496cf076b33
EthanMCox <ethancox@byu.edu>
2024-04-12 11:37:33 -0600

    Added placeholder code to game.jsx, added useStates in place of class properties for game

--- src/play/game.jsx
+++ src/play/game.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from 'react-bootstrap';
-import { delay } from './delay';
-import { GameEvent, GameNotifier } from './gameNotifier';
-import './play.css';
+import { Button } from "react-bootstrap";
+import { delay } from "./delay";
+import { GameEvent, GameEventNotifier } from "./gameNotifier";
+import "./play.css";
 
-export function makeAMatchGame(props) {
-    const userName = props.userName;
-    
+export function MakeAMatchGame(props) {
+  const userName = props.userName;
+  const cards = new Map();
+
+  const [allowPlayer, setAllowPlayer] = React.useState(true);
+  const [cardsflipped, setCardsFlipped] = React.useState([]);
+  const [cardsmatched, setCardsMatched] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [lives, setLives] = React.useState(10);
+  const [round, setRound] = React.useState(1);
+
+  return (
+    <div className="card-game-container">
+      <div className="card-game">
+        Card Placeholder
+      </div>
+    </div>
+  );
 }
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Players } from './players';
+import { MakeAMatchGame } from './game';
 
 export function Play(props) {
     return (
         <main className='bg-secondary'>
             <Players userName={props.userName} />
+            <MakeAMatchGame userName={props.userName} />
         </main>
     );
 }
