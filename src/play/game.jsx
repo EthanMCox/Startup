ad7099ccdc5ce1f695448804e64325fd7fa39047
EthanMCox <ethancox@byu.edu>
2024-04-12 11:50:04 -0600

    Added html stat bar, replacing values for React useState variables

--- src/play/game.jsx
+++ src/play/game.jsx
@@ -17,9 +17,59 @@ export function MakeAMatchGame(props) {
   const [round, setRound] = React.useState(1);
 
   return (
-    <div className="card-game-container">
-      <div className="card-game">
-        Card Placeholder
+    <div>
+      <div className="card-game-container">
+        <div className="card-game">Card Placeholder</div>
+      </div>
+
+      <div className="stat-bar container-fluid">
+        <div className="stat-bar-input">
+          <label for="restart"></label>
+        </div>
+        <div className="stat-bar-input">
+          <input
+            className="btn btn-outline-dark btn-sm"
+            type="button"
+            id="restart"
+            value="Restart"
+          />
+        </div>
+        <div>
+          <label for="round">Round: </label>
+        </div>
+        <div className="stat-bar-input">
+          <input
+            className="bg-dark text-light"
+            type="text"
+            id="round"
+            value={round}
+            readonly
+          />
+        </div>
+        <div>
+          <label for="lives">Lives: </label>
+        </div>
+        <div className="stat-bar-input">
+          <input
+            className="bg-dark text-light"
+            type="text"
+            id="lives"
+            value={lives}
+            readonly
+          />
+        </div>
+        <div>
+          <label for="score">Score: </label>
+        </div>
+        <div className="stat-bar-input">
+          <input
+            className="bg-dark text-light"
+            type="text"
+            id="score"
+            value={score}
+            readonly
+          />
+        </div>
       </div>
     </div>
   );
